2853ef4f8a307d68336fba7476272b852bee2a8b
lanou15ForthGroup <2641533671@qq.com>
2014-03-10 15:19:24 +0800

    demo

---
+++ QYMovieScanner/QYMovieScanner/QYAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  QYAppDelegate.h
+//  QYMovieScanner
+//
+//  Created by 范林松 on 14-3-10.
+//  Copyright (c) 2014年 freeDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QYAppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@property (retain, nonatomic) UIWindow *window;
+
+@end
---
+++ QYMovieScanner/QYMovieScanner/baseViews/QYBaseViewController.m
@@ -0,0 +1,38 @@
+//
+//  QYBaseViewController.m
+//  QYMovieScanner
+//
+//  Created by 范林松 on 14-3-10.
+//  Copyright (c) 2014年 freeDev. All rights reserved.
+//
+
+#import "QYBaseViewController.h"
+
+@interface QYBaseViewController ()
+
+@end
+
+@implementation QYBaseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
